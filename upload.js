6050c78b1c9a01e5aacfeb05562125a41c4ddb3f
anonymouspc <shyeyian@petalmail.com>
2025-12-01 14:16:34 +0800

    update before vsce publish

---
+++ upload.js
@@ -0,0 +1,11 @@
+let child_process = require("child_process")
+
+try {
+    child_process.execSync("git add .")
+    child_process.execSync("git commit -m 'update before vsce publish'")
+}
+catch (error) { 
+    // pass
+}
+
+child_process.execSync("vsce publish patch")

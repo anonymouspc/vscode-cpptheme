b91e47ba8701e84d478a0594eb1bab124073f3f9
anonymouspc <shyeyian@petalmail.com>
2025-12-03 11:09:24 +0800

    update

--- upload.js
+++ upload.js
@@ -2,7 +2,7 @@ let child_process = require("child_process")
 
 try {
     child_process.execSync("git add .")
-    child_process.execSync("git commit -m 'update before vsce publish'")
+    child_process.execSync("git commit -m update")
 }
 catch (error) { 
     // pass
